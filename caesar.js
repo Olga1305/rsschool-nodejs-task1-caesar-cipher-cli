57bc93a4fcfe7ed05979bdfaf31e1aec3855f137
Olga Dokukova <dokukova@gmail.com>
2021-05-08 23:08:56 +0200

    feat: add caesar.js file

---
+++ caesar.js
@@ -0,0 +1,19 @@
+const caesar = (string, shift) => {
+  return string
+    .split("")
+    .map((item) => {
+      if (!item.match(/^[a-zA-Z]*$/)) {
+        return item;
+      } else {
+        const code = item.charCodeAt(0);
+        if (code >= 65 && code <= 90) {
+          return (item = String.fromCharCode(((code - 65 + shift) % 26) + 65));
+        } else if (code >= 97 && code <= 122) {
+          return (item = String.fromCharCode(((code - 97 + shift) % 26) + 97));
+        }
+      }
+    })
+    .join("");
+};
+
+module.exports = { caesar };
