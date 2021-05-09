4cc62c2545455f4dc21b2e50499f9507f10e4074
Olga Dokukova <dokukova@gmail.com>
2021-05-09 20:43:22 +0200

    refactor: streams refactor

--- streams.js
+++ streams.js
@@ -8,10 +8,30 @@ const { caesar } = require("./caesar");
 const caesarPipeline = (options, cl) => {
   const { input, output } = options;
 
+  const creatReadInput = () => {
+    try {
+      return cl.stdin
+        ? process.stdin
+        : fs.createReadStream(path.resolve(input));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const createWriteOutput = () => {
+    try {
+      return cl.stdout
+        ? process.stdout
+        : fs.createWriteStream(path.resolve(output), { flags: "a" });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   const transformStream = new Transform({
     transform(chunk, encoding, callback) {
       try {
-        chunk = caesar(chunk.toString(), options);        
+        chunk = caesar(chunk.toString(), options);
         callback(null, chunk);
       } catch (err) {
         callback(err);
@@ -19,37 +39,12 @@ const caesarPipeline = (options, cl) => {
     },
   });
 
-  const filePipeline = () => {
-    pipeline(
-      fs.createReadStream(path.resolve(input)),
-      transformStream,
-      fs.createWriteStream(path.resolve(output), { flags: "a" }),
-      (err) => {
-        if (!err) {
-          console.log(chalk.bold.green(`Pipeline succeeded. Please, check the file ${output}`));
-        } else {
-          process.stderr.write(chalk.bold.red(`Pipeline failed with error ${err}`));
-          process.exit(1);
-        }
-      }
-    );
-  };
-
-  const clPipeline = () => {
-    pipeline(process.stdin, transformStream, process.stdout, (err) => {
-      if (err) {
-        process.stderr.write(chalk.bold.red(`Pipeline failed with error ${err}`));
-        process.exit(1);
-      } 
-    });
-  };
-
-  if (!cl.stdin && !cl.stdout) {
-    return filePipeline();
-  } else {
-    return clPipeline();
-  }
+  pipeline(creatReadInput(), transformStream, createWriteOutput(), (err) => {
+    if (err) {
+      process.stderr.write(chalk.bold.red(`Pipeline failed with error ${err}`));
+      process.exit(1);
+    }
+  });
 };
 
 module.exports = { caesarPipeline };
-
